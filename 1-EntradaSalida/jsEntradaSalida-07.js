0eb3e6dd072592406ddb0939d9002e21e555afec
Angel Aybar <angelaybar1996@gmail.com>
2021-01-30 17:29:25 -1100

    nuevos cambios

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,40 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+ 
+
+
 function sumar()
-{	
-	alert("ok");	
+{	var num1= parseInt(document.getElementById("txtIdNumeroUno").value,10);
+    var num2 = parseInt(document.getElementById("txtIdNumeroDos").value,10);
+	var resultado= num1 + num2;
+	alert("La suma es "+resultado);	
+	
 }
 
 function restar()
 {
+	var num1= parseInt(document.getElementById("txtIdNumeroUno").value,10);
+    var num2 = parseInt(document.getElementById("txtIdNumeroDos").value,10);
+	var resultado= num1 - num2;
+	alert("La resta es "+resultado);	
 	
 }
 
 function multiplicar()
 { 
-	
+	var num1= parseInt(document.getElementById("txtIdNumeroUno").value,10);
+    var num2 = parseInt(document.getElementById("txtIdNumeroDos").value,10);
+	var resultado= num1 * num2;
+	alert("La multiplicación es "+resultado);
 }
 
 function dividir()
 {
-	
+	var num1= parseInt(document.getElementById("txtIdNumeroUno").value,10);
+    var num2 = parseInt(document.getElementById("txtIdNumeroDos").value,10);
+	var resultado= num1 / num2;
+	alert("El cociente es "+resultado);
 }
 
