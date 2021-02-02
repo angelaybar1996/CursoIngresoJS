4bd13045e0bc809a6c28b7c1b2ed8615a2886632
Angel Aybar <angelaybar1996@gmail.com>
2021-02-01 23:04:59 -1100

    nuevos cambios

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,18 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+    var tempF;
+    var tempC;
+    tempF=parseFloat(document.getElementById("txtIdTemperatura").value);
+    tempC=(tempF-32)/1.8;
+    alert(tempF+" grados Fahrenheit son "+tempC+" grados Centigrados");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    var tempC;
+    var tempF;
+    tempC=parseFloat(document.getElementById("txtIdTemperatura").value);
+    tempF=tempC*1.8+32;
+    alert(tempC+" grados Centigrados son "+tempF+" grados Fahrenheit");
 }
